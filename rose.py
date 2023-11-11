6591c0730503fc483dff09cab4c2c21b6f69b7d2
Abishake01 <abishake381@gmail.com>
2023-11-11 10:27:30 +0530

    game

---
+++ rose.py
@@ -0,0 +1,14 @@
+import random
+options=["rock","paper","scissors"]
+user_choice=input("Enter your choice(rock,paper,or scissors):")
+computer_choice=random.choice("options")
+print("computer choose:")
+print(computer_choice)
+if(computer_choice==user_choice):
+	print("It's a tie!"or "It's a draw")
+elif("user_choice==rock"and"computer_choice==scissors")or("user_choice==rock"and"computer_choice==paper")or("user_choice==paper"and"computer_choice==Rock"):
+	print("you win the game")
+elif("user_choice==scissor"and"computer_choice==rock")or("user_choice==paper"and"computer_science==rock")or("user_choice==scissors"and"computer_choice==paper"):
+	print("Game is over please try again")
+else:
+	print("Computer is win the game!")
---
+++ rsp.py
@@ -0,0 +1,13 @@
+import random
+def play():
+    player=str(input("Rock->r,Papper->p,scissors->s")).lower()
+    choose=['r','s','p']
+    computer=random.choice(choose)
+    if (player==computer):
+        print("Match is draw")
+    elif((player=='r' and computer=='s')or(player=='r' and computer=='p')or(player=='s' and computer=='r')or(player=='s' and computer=='p')or(player=='p' and computer=='s')or(player=='p' and computer=='r')):
+        print("You Won the Match")
+    else:
+        print("You loose the Game!")
+    
+    
