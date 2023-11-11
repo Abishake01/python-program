b0568d3996107541e9a29124f2d2dd1c76a0d67f
Abishake01 <abishake381@gmail.com>
2023-11-11 10:44:18 +0530

    Run

--- rsp.py
+++ rsp.py
@@ -1,12 +1,10 @@
 import random
 def play():
-    player=str(input("Rock->r,Papper->p,scissors->s: ")).lower()
+    player=input("Rock->r,Papper->p,scissors->s: ").lower()
     choose=['r','s','p']
     computer=random.choice(choose)
     if (player==computer):
         print("Match is draw")
-    if(player!=choose):
-        print("Invalid Character...please choice correct one")
     elif((player=='r' and computer=='s')or(player=='r' and computer=='p')or(player=='s' and computer=='r')or(player=='s' and computer=='p')or(player=='p' and computer=='s')or(player=='p' and computer=='r')):
         print("You Won the Match")
     else:
