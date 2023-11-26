31d16ec5eed77d9cef45b3292a87cd499db02028
Abishake01 <abishake381@gmail.com>
2023-11-26 12:25:12 +0530

    CREATED

--- new/new/urls.py
+++ new/new/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for dpros project.
+URL configuration for new project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from heart.views import index,contact
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 urlpatterns = [
+    path('',index,name='index'),
+    path('contact/', contact ,name='contact'),
     path('admin/', admin.site.urls),
-    path("myapp/",include("myapp.urls"))
 ]
- 
-
