e096abc6d6f98369c5eb0f60170bdf83b049748d
Abishake01 <abishake381@gmail.com>
2024-04-25 19:26:00 +0530

    Number in Pyhton

---
+++ Learning/3num.py
@@ -0,0 +1,22 @@
+from math import *#Import all math function
+a=3
+b=4
+print(a+b)
+number=40
+number1=str(number)
+print(30+4-56)# check all maths in the print statement
+#print('number is '+number1)
+print('number is '+number1)#TypeError: can only concatenate str (not "int") to str
+print(max(4,2))#4
+print(abs(-3.4))#3.4
+print(min(2,68,1,6,376,33))#1
+#print(len(number)) TypeError: object of type 'int' has no len()
+print(round(6.2))#6
+print(round(3.5))#4
+print(bin(10))#0b1010
+#Math function
+print(sqrt(40))#6.324555320336759
+print(pow(4,3))#64.0
+print(sin(1/2))#0.479425538604203
+print(cos(1))#0.5403023058681398
+print(tan(1))#1.5574077246549023
