fc1a48ea5f7e768e6740f25db883eecdc090d817
Abishake01 <abishake381@gmail.com>
2024-04-25 21:47:29 +0530

    Get Input From Users

---
+++ Learning/4get_ip_user.py
@@ -0,0 +1,5 @@
+hero=input('Enter Your Fav Super Hero Name: ')
+age=input('Your super hero Age: ')# deffault it take a string...
+phone_no=int(input ("Enter a Phone number: ")) # it take integer value as a input
+print('My super Hero Name is '+hero+'\nhis age is '+age,'\nMobile number is',phone_no)
+#print(type(age)) Age also a string
