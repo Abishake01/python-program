fafc81d83a94f0024cfc6f44d1cdd4e52559cf1d
Abishake01 <abishake381@gmail.com>
2024-04-25 18:43:51 +0530

    start learning

---
+++ Learning/1var.py
@@ -0,0 +1,8 @@
+name='Abi'#name is a variable ,age,city also a variable
+age=20
+city='Avadi'
+print('Hi I Am',name,',Welcome to the club')# We can use the variable in between the statement
+print('I am',age,'years old')
+#print('I am',age,'years old'+name) string can attach another string
+#print('I am',age,'years old'+age) It shows an error because of a var is a integer
+print(city+' is my place')#use a variable in  before the statement and after the statement
