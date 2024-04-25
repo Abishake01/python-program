b98907246d0a7174ca8e7ae3400a76a9c07e5a66
Abishake01 <abishake381@gmail.com>
2024-04-25 21:56:39 +0530

    String replace

---
+++ Learning/5replace.py
@@ -0,0 +1,5 @@
+word=input('Enter a sentence: ')
+print('your sentence is '+word)
+rep1=input('Enter a word you want to replace: ')
+rep2=input('Enter a new word : ')
+print(word.replace(rep1,rep2))# a normal string replace
