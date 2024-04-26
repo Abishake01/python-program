386de8ac2aa7ce3c3328ba5f9698490e7cd612f4
Abishake01 <abishake381@gmail.com>
2024-04-26 21:59:12 +0530

    Tuples in Python

---
+++ Learning/7tuple.py
@@ -0,0 +1,13 @@
+# Tuple is immutable --> Unchangeable  we use ()brackets When we separate , is identify it is tuple
+name='a',1,True
+area=tuple(('adf'))
+print(name)#('a', 1)
+print(type(name))#<class 'tuple'>
+print(len(name))
+#name[0]='abi'
+print(name)#TypeError: 'tuple' object does not support item assignment
+print(type(name[0]))#<class 'str'>
+print(type(name[1]))#<class 'int'>
+print(type(name[2]))#<class 'bool'>
+print(type(area))#<class 'tuple'>
+
