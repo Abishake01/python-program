04b6968003ac2874114dc4de7a8ba5ca8cadf2e3
Abishake01 <abishake381@gmail.com>
2024-04-27 20:25:29 +0530

    Functions in Python

--- Learning/8function.py
+++ Learning/8function.py
@@ -1,13 +1,14 @@
 # Function in Python we use def()
-def v(name):# def variable(arguments):    Error->TypeError: v() missing 1 required positional argument: 'name'
-    print('Function Works ...'+name)
-v('abi')# it is call function 
+def v(name):# def variable(arguments):   
+    print('Function Works ...'+str(name))
+v(23)# it is call function.  Error-->TypeError: can only concatenate str (not "int") to str
+
 def add(a,b):
     c=a+b
     print(c)
 add(5,6)
 '''
-def v(name): 
+def v(name):  Error->TypeError: v() missing 1 required positional argument: 'name'
     print('Function Works ...'+name)
 v()
 def add(a,b):
