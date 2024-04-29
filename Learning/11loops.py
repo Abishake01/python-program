c17211c01dd082c582c848b8f154b3096af3f1d3
Abishake01 <abishake381@gmail.com>
2024-04-29 23:50:39 +0530

    While Loop in Pyhton

---
+++ Learning/11loops.py
@@ -0,0 +1,13 @@
+#While loop check the condition if the condition is true means it executed
+i=1
+
+while i<3:#it check the ccondition it is true means it will execute
+    print(i)
+    i+=1# increament 1 again check the condition untile the condition false
+
+while i<6 and i==1:
+    print(i)
+    i=i+1
+# it is a basic concept of while loop
+#-------------------------------------------------------------------------------------------------------------------------------------------------
+#For Loop have a (start,end,increment)
