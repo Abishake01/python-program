9c2f49c33ccb8b8e9bca6bd04f1398789f32b76d
Abishake01 <abishake381@gmail.com>
2024-05-03 22:49:40 +0530

    math module created

---
+++ Learning/prob.py
@@ -0,0 +1,20 @@
+class actor():
+        def __init__(self,name,age,movies):
+                self.name=name
+                self.age=age
+                self.movies=movies 
+                
+def maths():
+    print('Math Functions')
+def add(ele1,ele2):
+        result=ele1+ele2
+        print(result)
+def sub(ele1,ele2):
+        result=ele1-ele2
+        print(result)
+def mul(ele1,ele2):
+        result=ele1*ele2
+        print(result)
+def div(ele1,ele2):
+        result=ele1/ele2
+        print(result)
--- Learning/problem.py
+++ Learning/problem.py
@@ -1,5 +1,7 @@
-from math import *
-import math
+import  prob
+from prob import *
 var1=int(input('Enter a First Number: '))
 var2=int(input('Enter a Second Number: '))
-math.add(var1,var2)
+prob.add(var1,var2)
+cini=actor('Vijay',49,67)
+print(cini.age)
