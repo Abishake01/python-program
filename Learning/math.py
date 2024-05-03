6bf9cbc0ce539712c6dfd3b754da08a63597750a
Abishake01 <abishake381@gmail.com>
2024-05-03 22:37:05 +0530

    pacage created in python

---
+++ Learning/math.py
@@ -0,0 +1,14 @@
+class math_fun():
+    print('Math Functions')
+    def add(ele1,ele2):
+        result=ele1+ele2
+        print(result)
+    def sub(ele1,ele2):
+        result=ele1-ele2
+        print(result)
+    def mul(ele1,ele2):
+        result=ele1*ele2
+        print(result)
+    def div(ele1,ele2):
+        result=ele1/ele2
+        print(result)
---
+++ Learning/problem.py
@@ -0,0 +1,5 @@
+from math import *
+import math
+var1=int(input('Enter a First Number: '))
+var2=int(input('Enter a Second Number: '))
+math.add(var1,var2)
