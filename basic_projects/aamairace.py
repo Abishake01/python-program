77d602abd3d3e6f5d695499027119ba4f24a6de9
Abishake01 <abishake381@gmail.com>
2024-05-19 22:23:36 +0530

    Turtle Game

--- basic_projects/aamairace.py
+++ basic_projects/aamairace.py
@@ -1,8 +1,10 @@
 # Aamai race...
 import turtle
 import time
-HEIGHT,WIDTH=500,500
+import random
 
+HEIGHT,WIDTH=500,500
+COLORS=['red','blue','black','yellow','purple','pink','cyan','brown','orange','grey']
 def get_numberof_racers():
     racer=0
     while True:
@@ -22,16 +24,42 @@ def init_race():
     screen.setup(HEIGHT,WIDTH)
     screen.title('Aamai Race')
             
-racer=get_numberof_racers()
-print(racer)
+racers=get_numberof_racers()
+print(racers)
 init_race()
+random.shuffle(COLORS)
+colors=COLORS[:racers]
+print(colors)
+
+'''
+#basic turtle function
 racer=turtle.Turtle()
+
+racer.speed(1)#It is a speed of the turtle
+racer.penup()#It is used to remove a line turtle move...
+racer.shape('turtle')# It is a turtle shape shows...
+racer.color('yellow')#It is used for a color difference
 racer.forward(100)#forward and backward is a distance move
+racer.pendown()#it shows a line
 racer.left(90)#left right is a 90degree,45degree
 racer.forward(100)
 racer.right(90)
 racer.backward(100)
 racer.right(90)
 racer.forward(100)
-time.sleep(20)
  
+racer2=turtle.Turtle()
+
+racer2.speed(5)#It is a speed of the turtle
+racer2.penup()#It is used to remove a line turtle move...
+racer2.shape('turtle')# It is a turtle shape shows...
+racer2.color('brown')#It is used for a color difference
+racer2.forward(150)#forward and backward is a distance move
+racer2.pendown()#it shows a line
+racer2.left(90)#left right is a 90degree,45degree
+racer2.forward(100)
+racer2.right(90)
+racer2.backward(150)
+racer2.right(90)
+racer2.forward(150)
+'''
