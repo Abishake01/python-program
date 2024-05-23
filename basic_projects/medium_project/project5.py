5057d71821e9c24e2e2e8bfcd7cd4efe06fe1b73
Abishake01 <abishake381@gmail.com>
2024-05-23 22:28:01 +0530

    Type Testing

--- basic_projects/medium_project/project5.py
+++ basic_projects/medium_project/project5.py
@@ -1,1 +1,11 @@
 #Typing Test
+
+import curses
+from curses import wrapper
+def main(stdscr):
+    stdscr.clear()
+    stdscr.addstr('Hello Welcome!')
+    stdscr.refresh()
+    stdscr.getkey()
+    
+wrapper(main)
