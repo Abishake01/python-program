520999ed7fb07f1e2a2d9f3a64e66f008ee8cdac
Abishake01 <abishake381@gmail.com>
2024-05-28 22:24:16 +0530

    Speed

--- basic_projects/medium_project/project5.py
+++ basic_projects/medium_project/project5.py
@@ -2,10 +2,39 @@
 
 import curses
 from curses import wrapper
-def main(stdscr):
+
+def start_screen(stdscr):
     stdscr.clear()
-    stdscr.addstr('Hello Welcome!')
-    stdscr.refresh()
+    stdscr.addstr('Welcome To the Speed Typing Test!')
+    stdscr.addstr('\nPress any key to begin!')
+    stdscr.refresh() 
     stdscr.getkey()
+    
+def wpm_test(stdscr):
+    target_text='Hello World this is some test text for this app!'
+    current_text=[]
+    stdscr.clear()
+    stdscr.addstr(target_text)
+    stdscr.refresh() 
+    
+    while True:
+        key=stdscr.getkey()
+        current_text.append(key)
+        
+        stdscr.clear()
+        stdscr.addstr(target_text)
+        for char in current_text:
+            stdscr.addstr(char,curses.color_pair(1))
+        stdscr.refresh()
+def main(stdscr):
+    
+    curses.init_pair(1,curses.COLOR_GREEN,curses.COLOR_BLACK)#curses.init_pair(number,text color,background color)
+    curses.init_pair(2,curses.COLOR_RED,curses.COLOR_BLACK)
+    curses.init_pair(3,curses.COLOR_WHITE,curses.COLOR_BLACK)
+    #stdscr.addstr(row ,col,'string')
+    #stdscr.addstr('Hello Welcome!',curses.color_pair(1))
+    start_screen(stdscr)
+    wpm_test(stdscr)
+     
     
 wrapper(main)
